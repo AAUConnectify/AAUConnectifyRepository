f141d36edb5c16ceb74e166eaf04d980aa01ef58
EMRAN YONAS YIMER <emranyonas602@gmail.com>
2024-01-12 18:38:04 +0300

    register page get done

--- src/pages/Anouncement/script.js
+++ src/pages/Anouncement/script.js
@@ -1,9 +1,12 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var navToggle = document.getElementById('nav-toggle');
-    var navLinks = document.getElementById('nav-links');
-    if (navToggle && navLinks) {
-        navToggle.addEventListener('click', function () {
-            navLinks.classList.toggle('hidden');
-        });
-    }
+if (!localStorage.getItem("userData")) {
+  window.location.href = "../sign_up/index.html";
+}
+document.addEventListener("DOMContentLoaded", function () {
+  var navToggle = document.getElementById("nav-toggle");
+  var navLinks = document.getElementById("nav-links");
+  if (navToggle && navLinks) {
+    navToggle.addEventListener("click", function () {
+      navLinks.classList.toggle("hidden");
+    });
+  }
 });
