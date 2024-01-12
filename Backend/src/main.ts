ed085c9c4d8f9f0674ed7b15cd67a8e7a20a8994
EMRAN YONAS YIMER <emranyonas602@gmail.com>
2024-01-12 22:41:43 +0300

    Some Change

--- Backend/src/main.ts
+++ Backend/src/main.ts
@@ -4,15 +4,20 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Enable CORS
+  app.enableCors();
+
   const options = new DocumentBuilder()
-  .setTitle('AAU -Announcement API')
-  .setDescription('test  api')
-  .setVersion('1.0')
-  .build();
+    .setTitle('AAU -Announcement API')
+    .setDescription('test  api')
+    .setVersion('1.0')
+    .build();
 
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(5000);
+  await app.listen(5011);
 }
 bootstrap();
+
